0b0d8ff483bc25ce19d7ba4c30abb79f3cac40bf
alittlefield516 <42576478+alittlefield516@users.noreply.github.com>
2018-12-13 11:41:17 -0500

    Create copyIssueIDs.js

---
+++ copyIssueIDs.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Copy ID and PR
+// @namespace    https://www.monetate.com/
+// @version      1.0
+// @description  Adds a context menu item to copy the issue and pull request IDs from a JIRA ticket.
+// @author       Andrew Littlefield
+// @match        https://monetate.atlassian.net/browse/*
+// @grant        none
+// @run-at       context-menu
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const prLink = document.querySelector('#customfield_10700-val > a');
+    const issueLink = document.querySelector('#key-val');
+    let toCopy = '';
+    if(prLink) {
+        toCopy += 'PR' + prLink.href.match(/[0-9]*5/) + ' - ';
+    }
+    toCopy += issueLink.getAttribute('data-issue-key');
+
+    let dummyText = document.createElement('textarea');
+    dummyText.value = toCopy;
+    document.body.appendChild(dummyText);
+    dummyText.select();
+    document.execCommand('copy');
+    document.body.removeChild(dummyText);
+})();
